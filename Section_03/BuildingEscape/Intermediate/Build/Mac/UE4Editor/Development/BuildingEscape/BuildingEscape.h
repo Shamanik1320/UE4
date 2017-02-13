44f82529132993df364ebeab293f3c368fc39451
tristanmcguire <tristanmcguire@gmail.com>
2017-02-13 07:46:04 +0100

    sec3init

---
+++ Section_03/BuildingEscape/Intermediate/Build/Mac/UE4Editor/Development/BuildingEscape/BuildingEscape.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_BUILDINGESCAPE_H__
+#define __AUTO_BUILDINGESCAPE_H__
+//Last Write: 13/02/2017 06:30:44 +00:00
+#include "/Users/Tristan/Documents/Git/UE4/Section_03/BuildingEscape/Source/BuildingEscape/BuildingEscape.h"
+#endif//__AUTO_BUILDINGESCAPE_H__
